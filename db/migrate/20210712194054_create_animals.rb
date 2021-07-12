199bf8797a82f1a21135c37ff7043e3e648874c1
Justin <jdanielmcarthur@msn.com>
2021-07-12 13:34:06 -0700

    filled out table columns and ran rake db:migrate, tables have been created and schema.rb file is loaded

--- db/migrate/20210712194054_create_animals.rb
+++ db/migrate/20210712194054_create_animals.rb
@@ -1,4 +1,10 @@
 class CreateAnimals < ActiveRecord::Migration[6.1]
   def change
+    create_table :animals do |t|
+      t.string :name
+      t.integer :age
+      t.string :breed
+      t.integer :shelter_id
+    end
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_07_12_194124) do
+
+  create_table "adoptions", force: :cascade do |t|
+    t.string "date"
+    t.integer "price"
+    t.integer "animal_id"
+    t.integer "visitor_id"
+  end
+
+  create_table "animals", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "breed"
+    t.integer "shelter_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.integer "zip_code"
+  end
+
+  create_table "shelters", force: :cascade do |t|
+    t.string "name"
+    t.string "location_id"
+  end
+
+  create_table "visitors", force: :cascade do |t|
+    t.string "name"
+    t.integer "location_id"
+  end
+
+end
