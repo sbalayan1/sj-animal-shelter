d276d70789d2206186949adc889ce3fc72a114f5
Justin <jdanielmcarthur@msn.com>
2021-07-14 14:04:48 -0700

    added post params to visitors and adoptions

--- apps/application.rb
+++ apps/application.rb
@@ -4,12 +4,20 @@ class Application
       resp = Rack::Response.new
       req = Rack::Request.new(env)
 
+
+
+
       if req.path.match(/animals/) && req.get?
         animals = Animal.all.map do |animal|
           {id: animal.id, species: animal.species, name: animal.name, age: animal.age, breed: animal.breed, adopted: animal.adopted, shelter_id: animal.shelter_id}
         end
 
         return [200, { 'Content-Type' => 'application/json' }, [ {:animals => animals}.to_json ]]  
+
+
+
+
+
 
       elsif req.path.match(/shelters/) && req.get?
         shelters = Shelter.all.map do |shelter|
@@ -18,12 +26,31 @@ class Application
 
         return [200, { 'Content-Type' => 'application/json' }, [ {:shelters => shelters}.to_json ]]  
 
+
+
+
+
+
       elsif req.path.match(/visitors/) && req.get?
         visitors = Visitor.all.map do |visitor|
             {id: visitor.id, name: visitor.name, password: visitor.password, location_id: visitor.location_id}
         end
 
         return [200, { 'Content-Type' => 'application/json' }, [ {:visitors => visitors}.to_json ]] 
+
+
+      elsif req.path.match(/visitors/) && req.post?
+        data = JSON.parse req.body.read
+        visitor = Visitor.create(name: data["name"], password: data["password"], location_id: data["location_id"])
+
+        res_visitor = {id: visitor.id, name: visitor.name, password: visitor.password, location_id: visitor.location_id}
+        end  
+
+        return [200, { 'Content-Type' => 'application/json' }, [ {:visitor => res_visitor}.to_json ]]
+
+
+
+
 
       elsif req.path.match(/adoptions/) && req.get?
         adoptions = Adoption.all.map do |adoption|
@@ -32,6 +59,20 @@ class Application
 
         return [200, { 'Content-Type' => 'application/json' }, [ {:adoptions => adoptions}.to_json ]] 
 
+
+      elsif req.path.match(/adoptions/) && req.post?
+        data = JSON.parse req.body.read
+        adoption = Adoption.create(date: data["date"], price: data["price"], animal_id: data["animal_id"], visitor_id: data["visitor_id"])
+
+        res_adoption = {id: adoption.id, date: adoption.date, price: adoption.price, animal_id: adoption.animal_id, visitor_id: adoption.visitor_id}
+        end  
+
+        return [200, { 'Content-Type' => 'application/json' }, [ {:adoption => res_adoption}.to_json ]]
+
+
+
+
+
       elsif req.path.match(/locations/) && req.get?
         locations = Location.all.map do |location|
             {id: location.id, zip_code: location.zip_code}
@@ -39,6 +80,11 @@ class Application
 
         return [200, { 'Content-Type' => 'application/json' }, [ {:locations => locations}.to_json ]]
         
+
+
+
+
+
       else
         resp.write "Path Not Found"
       end
